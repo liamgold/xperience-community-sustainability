61a05e7c751a2136627df3fe9d2eb30e81193f6f
Liam Goldfinch <liam.goldfinch@idhl.co.uk>
2025-05-13 00:17:05 +0100

    Dashboard will be done later, so removing it for now.

--- src/XperienceCommunity.Sustainability/UIPages/SustainabilityDashboard/SustainabilityDashboard.cs
+++ src/XperienceCommunity.Sustainability/UIPages/SustainabilityDashboard/SustainabilityDashboard.cs
@@ -2,14 +2,14 @@
 using XperienceCommunity.Sustainability;
 using XperienceCommunity.Sustainability.Admin;
 
-[assembly: UIApplication(
-    identifier: "Goldfinch.Sustainability",
-    type: typeof(SustainabilityDashboard),
-    slug: "sustainability",
-    name: "Sustainability",
-    category: SustainabilityAdminModule.CUSTOM_CATEGORY,
-    icon: Icons.Earth,
-    templateName: "@sustainability/web-admin/SustainabilityDashboard")]
+//[assembly: UIApplication(
+//    identifier: "XperienceCommunity.Sustainability",
+//    type: typeof(SustainabilityDashboard),
+//    slug: "sustainability",
+//    name: "Sustainability",
+//    category: SustainabilityAdminModule.CUSTOM_CATEGORY,
+//    icon: Icons.Earth,
+//    templateName: "@sustainability/web-admin/SustainabilityDashboard")]
 
 namespace XperienceCommunity.Sustainability.Admin
 {
